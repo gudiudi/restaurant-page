59d5f6591124652a3dbb6c36bc4c0f11acefd684
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-03-17 18:12:43 +0700

    feat: add logo in home component

--- src/home.js
+++ src/home.js
@@ -1,7 +1,21 @@
+import Logo from './assets/logo.png';
+
 export default function home() {
-  const div = document.createElement('div');
-  div.classList = 'content-desc'
-  div.style = 'text-align: center;'
-  div.textContent = 'Forno is a Helsinki-based pizzeria where artisan craftsmanship meets premium ingredients.\nOur pizzas are made with a slow-fermented, flavorful dough and topped with the finest locally sourced ingredients.'
-  return div;
+  const main = document.createElement('div');
+  main.classList = 'main';
+
+  const logo = document.createElement('div');
+  const img = new Image();
+  img.src = Logo;
+
+  const desc = document.createElement('div');
+  desc.classList = 'desc'
+  desc.style = 'text-align: center;'
+  desc.textContent = 'Forno is a Helsinki-based pizzeria where artisan craftsmanship meets premium ingredients.\nOur pizzas are made with a slow-fermented, flavorful dough and topped with the finest locally sourced ingredients.'
+  
+  logo.appendChild(img);
+  main.appendChild(logo);
+  main.appendChild(desc);
+
+  return main;
 }
